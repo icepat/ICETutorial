216c81bdfdd355c390ae5b31bf719f4a8b521eae
Patrick <patrick.trillsam@gmail.com>
2014-06-19 18:45:16 +1000

    Cleanup.

--- ICETutorial/Libraries/ICETutorialController.h
+++ ICETutorial/Libraries/ICETutorialController.h
@@ -12,12 +12,12 @@
 
 #define TUTORIAL_LABEL_TEXT_COLOR               [UIColor whiteColor]
 #define TUTORIAL_LABEL_HEIGHT                   45
-#define TUTORIAL_TITLE_FONT                 [UIFont fontWithName:@"Helvetica-Bold" size:17.0f]
-#define TUTORIAL_TITLE_LINES_NUMBER         1
-#define TUTORIAL_TITLE_OFFSET               180
-#define TUTORIAL_SUB_TITLE_FONT                      [UIFont fontWithName:@"Helvetica" size:15.0f]
-#define TUTORIAL_SUB_TITLE_LINES_NUMBER              2
-#define TUTORIAL_SUB_TITLE_OFFSET                    150
+#define TUTORIAL_TITLE_FONT                     [UIFont fontWithName:@"Helvetica-Bold" size:17.0f]
+#define TUTORIAL_TITLE_LINES_NUMBER             1
+#define TUTORIAL_TITLE_OFFSET                   180
+#define TUTORIAL_SUB_TITLE_FONT                 [UIFont fontWithName:@"Helvetica" size:15.0f]
+#define TUTORIAL_SUB_TITLE_LINES_NUMBER         2
+#define TUTORIAL_SUB_TITLE_OFFSET               150
 
 // Scrolling state.
 typedef NS_OPTIONS(NSUInteger, ScrollingState) {
